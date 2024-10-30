27166cfa9523c69f45e42fdbcb9ae449c0d66c4d
JuliansCastro <juacastropa@unal.edu.co>
2024-10-30 15:32:58 -0500

    FileCSV class documentation added (filewriter)

--- Modules/filewriter.py
+++ Modules/filewriter.py
@@ -21,6 +21,23 @@ import csv
 import os
 
 class FileCSV():
+    '''
+    A class for creating and managing CSV files for data storage.
+
+    This class initializes a CSV file with a specific naming convention
+    based on the provided type and frequency. It provides functionality 
+    to save data  to the CSV file, ensuring  that the header is written 
+    only when the file is created.
+
+    Args:
+        name (str): The base name for the CSV file.
+        frequency: The frequency value used in the filename for certain types.
+        header: The header row to be written to the CSV file.
+        type (str): The type of file being created, which determines the filename format.
+
+    Raises:
+        ValueError: If an invalid file type is provided.
+    '''
     def __init__(self, name:str, frequency, header, type:str) -> None:
         self.DATETIME = dt.now().strftime('%d-%m-%Y-%H-%M-%S')
         match type:
@@ -46,7 +63,19 @@ class FileCSV():
         self.header = header
 
     def saveData(self, data) -> None:
+        '''
+        Saves data to a CSV file, creating the file if it does not exist.
 
+        This function checks if the specified file already exists and opens
+        it in append mode if it does, or in write mode if it does not. It writes
+        the header row if the file is newly created and appends the provided data as a new row.
+
+        Args:
+            data (list): The data to be saved as a new row in the CSV file.
+
+        Returns:
+            None
+        '''
         self.file_exist = os.path.isfile(self.filename)
 
         with open(self.filename, mode = "a" if self.file_exist else "w", newline="") as file:
